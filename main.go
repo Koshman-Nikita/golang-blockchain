4f869aee7c5d962da9be41b67dfadda04608ef87
Koshman-Nikita <63288899+Koshman-Nikita@users.noreply.github.com>
2023-05-30 15:16:18 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"os"
 
-	"github.com/Koshman-Nikita/golang-blockchain//cli"
+	"github.com/Koshman-Nikita/golang-blockchain/cli"
 )
 
 func main() {
 	defer os.Exit(0)
+
 	cmd := cli.CommandLine{}
 	cmd.Run()
 }
